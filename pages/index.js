c95a0e94aec29e33221d5b84b18d0c15c5b8dab0
shannonfarvolden <shannonfarvolden@hotmail.com>
2019-02-14 01:01:34 -0800

    Site setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../lib/Layout";
+
+const Index = () => (
+  <Layout>
+    <h3>Stock Visualizer</h3>
+    <input type="text" />
+    <button>Search</button>
+  </Layout>
+);
+
+export default Index;
